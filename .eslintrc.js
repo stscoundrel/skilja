d0bdb4148204d89d9709f0eeceb0496bd4f9b85e
stscoundrel <sampo@pixels.fi>
2020-08-23 11:15:46 +0300

    Add dependencies & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    node: true,
+    es6: true,
+  },
+  globals: {
+    document: true,
+  },
+  rules: {
+    semi: 0,
+    'no-console': 0,
+    'consistent-return': 0,
+    'no-use-before-define': 0,
+  },
+}
